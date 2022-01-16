26119d63ece7f73928d0eef40aff6b5cb8a84b27
Hana Stupková <hstupkova@gmail.com>
2022-01-16 16:39:32 +0100

    type guards

--- src/app.ts
+++ src/app.ts
@@ -41,3 +41,57 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') { // = type guard
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log('Name: ' + emp.name);
+  if ('privileges' in emp) {
+    console.log('Privileges: ' + emp.privileges);
+  }
+  if ('startDate' in emp) {
+    console.log('Start date: ' + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+printEmployeeInformation({name: 'Manu', startDate: new Date()});
+
+class Car {
+  drive() {
+    console.log('Driving...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck.');
+  }
+
+  loadCargo(amount: number) {
+    console.log('Loading cargo: ' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // if ('loadCargo' in vehicle) { // not a bad way but less elegant
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(500);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
